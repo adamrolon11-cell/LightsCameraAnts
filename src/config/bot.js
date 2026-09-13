b5f5d7d56f5dd86df1c0573ef06c1b966692cf3c
adamrolon11-cell <adamrolon11@gmail.com>
2026-09-13 12:34:50 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "LightCameraAnts community", // required by Discord API, not shown in the client
         state: "Listening",     // this is what people actually see
-        type: 2,               // Custom
+        type: 3,               // Custom
       },
     ],
   },
